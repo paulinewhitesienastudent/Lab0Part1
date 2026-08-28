b75679e4be78b2a87c62cc7277a07a01344fe06a
Pauline White <47893133+paulinewhitesiena@users.noreply.github.com>
2026-08-28 10:16:37 -0400

    Add files via upload

---
+++ Hello210.java
@@ -0,0 +1,13 @@
+
+/**
+ * CSIS 210 Setup Lab "Hello, World" program
+ *
+ * @author Prof. White
+ */
+public class Hello210 {
+    
+    public static void main(String args[]) {
+        
+        System.out.println("Hello CSIS 210 World!");
+    }
+}
