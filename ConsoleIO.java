3dfec013e1b73efb3efd4bee5940abdc0647b381
paulinewhitesienastudent <paulinewhite72@gmail.com>
2026-08-28 11:04:13 -0400

    Practice program.

---
+++ ConsoleIO.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+/**
+ * Console I/O example
+ *
+ * @author paulinewhitesienastudent
+*/
+public class ConsoleIO 
+{
+    public static void main(String args[]) 
+    {
+	Scanner keyboard = new Scanner(System.in);
+	System.out.println("What is your name?");
+	String name = keyboard.nextLine();
+	keyboard.close();
+	System.out.println(name + " says Java is fun.");
+    }
+}
